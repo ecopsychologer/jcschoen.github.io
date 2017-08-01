b415ac58ad4c69c66baee9711b7616070bbe3971
Jack Schoen <schoenjack98@gmail.com>
2017-08-01 10:11:50 -0400

    more prism
    hopefully this will allow me to have syntax code

---
+++ test.js
@@ -0,0 +1,9 @@
+var test = 1;
+function testing (subject) {
+    console.log("Testing, testing ");
+    while(subject<=3) {
+        console.log(subject);
+        subject++;
+    }
+}
+testing(test);
