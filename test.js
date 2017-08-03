b26f93688eaf07b0b719abf83a77485947e02e51
Jack Schoen <schoenjack98@gmail.com>
2017-08-03 16:13:13 -0400

    new

--- test.js
+++
@@ -1,9 +0,0 @@
-var test = 1;
-function testing (subject) {
-    console.log("Testing, testing ");
-    while(subject<=3) {
-        console.log(subject);
-        subject++;
-    }
-}
-testing(test);
