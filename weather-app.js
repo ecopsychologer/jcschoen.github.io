c7c192cf274eab6213c7e8236e89a2c069a85e0a
Jack Schoen <schoenjack98@gmail.com>
2017-08-28 19:27:30 -0400

    added a functionality and font
    added a javascript weather app using the dark sky api, and the google
    roboto font

---
+++ weather-app.js
@@ -0,0 +1,28 @@
+function weather() {
+
+  var location = document.getElementById("location");
+  var apiKey = prompt("Please enter your API key", "API Key Here");
+  var url = 'https://api.forecast.io/forecast/';
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    location.innerHTML = 'Latitude is ' + latitude + '° Longitude is ' + longitude + '°';
+
+    $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
+      $('#temp').html(data.currently.temperature + '° F');
+      $('#minutely').html(data.minutely.summary);
+    });
+  }
+
+  function error() {
+    location.innerHTML = "Unable to retrieve your location";
+  }
+
+  location.innerHTML = "Locating...";
+}
+
+weather();
