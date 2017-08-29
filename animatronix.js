eaae66b5f3c153a55fdf6069f9af3cdf12feeefe
Jack Schoen <schoenjack98@gmail.com>
2017-08-28 23:17:59 -0400

    save api key as cookie
    a test, to not breach the TOS of dark sky

---
+++ animatronix.js
@@ -0,0 +1,17 @@
+// Original JavaScript code by Chirp Internet: www.chirp.com.au
+  // Please acknowledge use of this code by including this header.
+
+  var today = new Date();
+  var expiry = new Date(today.getTime() + 30 * 24 * 3600 * 1000); // plus 30 days
+
+  function setCookie(name, value)
+  {
+    document.cookie=name + "=" + escape(value) + "; path=/; expires=" + expiry.toGMTString();
+  }
+
+  function putCookie(form)
+  {
+   setCookie("api", form[0].api.value);
+
+    return true;
+  }
